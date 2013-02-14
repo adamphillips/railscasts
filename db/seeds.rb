68eefd2e0c30c60cc21f76caaf2272855d323e74
Adam Phillips <aphillips@scholastic.co.uk>
2013-02-14 11:40:49 +0000

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+episodes = Episode.create([
+  {:name => 'Episode 1', :published_at => Time.now - 5.days, :seconds => 300},
+  {:name => 'Episode 2', :published_at => Time.now - 4.days, :seconds => 320},
+  {:name => 'Episode 3', :published_at => Time.now - 3.days, :seconds => 180},
+  {:name => 'Episode 4', :published_at => Time.now - 2.days, :seconds => 200},
+  {:name => 'Episode 5', :published_at => Time.now - 1.days, :seconds => 400}
+])
